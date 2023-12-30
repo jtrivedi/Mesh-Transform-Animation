fdbac5e70d180496f5ba95b90df4a7d156e84fd9
Janum Trivedi <jtrivedi@me.com>
2023-12-30 11:19:31 -0800

    Initial Commit

---
+++ MeshTransformAnimation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MeshTransformAnimation
+//
+//  Created by Janum Trivedi on 12/30/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MeshTransformAnimation/MeshTransformAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeshTransformAnimationApp.swift
+//  MeshTransformAnimation
+//
+//  Created by Janum Trivedi on 12/30/23.
+//
+
+import SwiftUI
+
+@main
+struct MeshTransformAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
